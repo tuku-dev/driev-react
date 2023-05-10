74940f9ea1c1e8790b4e6dbaa0d67db4b3a0eb1b
Satrughna <thisissatrughna@gmail.com>
2023-05-10 15:23:30 +0530

    changes

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Header from "../../shared/header/Header";
+import BookADrive from "./sections/book-a-ride/BookADrive";
+import Logo from "../../assets/imgs/driEV.svg";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <div className="banner">
-        Hello
+      <div className="home-page">
+        <div className="hero-section pb-4 mb-3">
+          <div className="container px-3 px-lg-1">
+            <div className="section-title-container text-center">
+              <h3 className="text-white section-title d-inline-flex align-items-baseline justify-content-center">
+                <span>Book a</span>
+                <img src={Logo} alt="" />
+                <span className="bottom-border"></span>
+              </h3>
+            </div>
+            <BookADrive />
+          </div>
+          <div className="container px-3 px-lg-1"></div>
+        </div>
       </div>
     </>
   );
---
+++ src/pages/home/sections/book-a-ride/BookADrive.js
@@ -0,0 +1,72 @@
+import Plus from "../../../../assets/imgs/plus-icon.svg";
+import Minus from "../../../../assets/imgs/minus-icon.svg";
+
+export default function BookADrive() {
+  return (
+    <form>
+      <div className="search-form-container align-items-end">
+        <div className="city-section d-flex flex-column">
+          <label className="form-label text-white">Search your City</label>
+          <input
+            type="text"
+            className="form-control search-city search-input"
+          />
+        </div>
+        <div className="rental-type-section">
+          <label className="form-label text-white">Rental Type</label>
+          <select
+            name="rentalType"
+            id="rentalType"
+            className="form-select search-input search-type"
+            required
+          >
+            <option value="">Select</option>
+            <option value="daily">Daily</option>
+            <option value="weekly">Weekly</option>
+            <option value="subscription">Subscription</option>
+          </select>
+        </div>
+        <div className="duration-section">
+          <label className="form-label text-white">Select Duration</label>
+          <div className="search-input d-flex align-items-center gap-2">
+            <input
+              type="text"
+              className="select-duration"
+              placeholder="Day(s)"
+            />
+            <div className="controls d-flex gap-2 ms-auto">
+              <span className="d-inline-block">
+                <img src={Minus} alt="Minus" />
+              </span>
+              <span className="value">00</span>
+              <span className="d-inline-block">
+                <img src={Plus} alt="Plus" />
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="date-time-section">
+          <label className="form-label text-white">Choose Date & Time</label>
+          <div className="search-input align-items-center gap-2">
+            <input
+              type="text"
+              className="choose-date text-primary"
+              readOnly
+              placeholder="DD / MM / YYYY"
+            />
+            <input
+              type="text"
+              className="choose-time text-primary"
+              readOnly
+              placeholder="HH : MM hrs"
+            />
+          </div>
+        </div>
+        <div className="proceed-section">
+          <button className="button">Proceed</button>
+          {/* To disable this add class disabled */}
+        </div>
+      </div>
+    </form>
+  );
+}
